5ee222e62f7d2abaf4b3969a9b2e3b7c93778417
Claudio Coppari <claudiocup@gmail.com>
2019-02-23 22:01:45 +0100

    Enforce running docker as the current user

--- ng-init.sh
+++ ng-init.sh
@@ -2,4 +2,7 @@
 
 PROJECT_NAME=hello-angular
 
-docker run -v $PWD:/app/ node-ng:7.3.1-cli /bin/bash -c "cd /app && ng new $PROJECT_NAME --style=scss"
+docker run -v $PWD:/app/            \
+           --user $(id -u):$(id -g) \
+           node-ng:7.3.1-cli        \
+           /bin/bash -c "cd /app && ng new $PROJECT_NAME --style=scss"
--- ng-serve.sh
+++ ng-serve.sh
@@ -2,4 +2,8 @@
 
 PROJECT_NAME=hello-angular
 
-docker run -p 4200:4200 -v $PWD/$PROJECT_NAME:/app/ node-ng:7.3.1-cli /bin/bash -c "cd /app && ng serve --host 0.0.0.0"
+docker run -p 4200:4200                \
+           -v $PWD/$PROJECT_NAME:/app/ \
+           --user $(id -u):$(id -g)    \
+           node-ng:7.3.1-cli           \
+           /bin/bash -c "cd /app && ng serve --host 0.0.0.0"
