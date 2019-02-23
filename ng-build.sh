ecc0c0038ec365ad01496e14932a291f08c7df51
Claudio Coppari <claudiocup@gmail.com>
2019-02-23 22:01:33 +0100

    Enforce running docker as the current user

--- ng-build.sh
+++ ng-build.sh
@@ -2,4 +2,8 @@
 
 PROJECT_NAME=hello-angular
 
-docker run -v $PWD/$PROJECT_NAME:/app/ node-ng:7.3.1-cli /bin/bash -c "cd /app && ng build --prod"
+docker run                       \
+     -v $PWD/$PROJECT_NAME:/app/ \
+     --user $(id -u):$(id -g)    \
+     node-ng:7.3.1-cli           \
+     /bin/bash -c "cd /app && ng build --prod"
